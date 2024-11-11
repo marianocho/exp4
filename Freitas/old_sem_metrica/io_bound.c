3ad7889e04eb867627d12b7609ff90149408f15f
Mariano Cho <mariano.cho@conpec.com.br>
2024-11-10 21:06:51 -0300

    3 metricas

--- Freitas/old_sem_metrica/io_bound.c
+++ Freitas/old_sem_metrica/io_bound.c
@@ -7,36 +7,72 @@
 #define LINE_LENGTH 100
 
 // Função simples para gerar números pseudo-aleatórios
-unsigned int custom_rand() {
-    static unsigned int seed = 123456789;  // Semente fixa para reproduzir os resultados
+unsigned int custom_rand()
+{
+    static unsigned int seed = 123456789; // Semente fixa para reproduzir os resultados
     seed = seed * 1103515245 + 12345;
     return (seed / 65536) % 32768;
 }
 
 // Gera uma linha aleatória de 100 caracteres
-void generate_random_line(char *line) {
-    for (int i = 0; i < LINE_LENGTH - 1; i++) {
-        line[i] = 'A' + (custom_rand() % 26);  // Letras aleatórias
+void generate_random_line(char *line)
+{
+    for (int i = 0; i < LINE_LENGTH - 1; i++)
+    {
+        line[i] = 'A' + (custom_rand() % 26); // Letras aleatórias
     }
     line[LINE_LENGTH - 1] = '\0';
 }
 
-void perform_io_operations() {
+void perform_io_operations()
+{
     int fd = open("testfile.txt", O_CREATE | O_RDWR);
     char line[LINE_LENGTH];
+    int t_start, t_end, lat;
+    int total_lat = 0 int min_lat = 1000000;
+    int max_lat = 0;
+    int latencies[NUM_LINES];
 
-    for (int i = 0; i < NUM_LINES; i++) {
+    for (int i = 0; i < NUM_LINES; i++)
+    {
         generate_random_line(line);
+
+        // Calculando tempos
+        t_start = uptime();
         write(fd, line, strlen(line));
+        t_end = uptime();
+        lat = t_end - t_start;
+        latencies[i] = lat;
+        total_lat += lat;
+
+        // Atualizando latencias max e min
+        if (lat > max_lat)
+        {
+            max_lat = lat;
+        }
+        if (lat < min_lat)
+        {
+            min_lat = lat;
+        }
     }
 
     // Realizar permutações e exclusão do arquivo
     close(fd);
     unlink("testfile.txt");
+
+    //
+    int avg_lat = total_lat / NUM_LINES;
+    for (int i = 0; i < NUM_LINES; i++)
+    {
+        float normal_lat = 1.0 - ((float)(latencies[i] - min_lat) / (max_lat - min_lat));
+        printf(1, "Operação %d: Latência Normalizada = %d -> %.2f\n", i + 1, latencies[i], normalized_latency);
+    }
 }
 
-int main() {
-    for (int i = 0; i < 30; i++) {
+int main()
+{
+    for (int i = 0; i < 30; i++)
+    {
         perform_io_operations();
     }
     exit();
--- Freitas/old_sem_metrica/run_experiment.c
+++ Freitas/old_sem_metrica/run_experiment.c
@@ -3,37 +3,92 @@
 #include "../stat.h"
 #include "../fcntl.h"
 
-void run_experiment(int cpu_bound_procs, int io_bound_procs) {
-    for (int i = 0; i < cpu_bound_procs; i++) {
-        if (fork() == 0) {
+int run_experiment(int cpu_bound_procs, int io_bound_procs, int *total_exec_times)
+{
+    int t_start, t_end;
+    int total_process = cpu_bound_procs + io_bound_procs;
+    int pid;
+
+    t_start = uptime();
+    for (int i = 0; i < cpu_bound_procs; i++)
+    {
+        pid = fork();
+        if (pid == 0)
+        {
             exec("cpu_bound", 0);
             exit();
         }
+        total_exec_times[i] = uptime() - t_start;
     }
 
-    for (int i = 0; i < io_bound_procs; i++) {
-        if (fork() == 0) {
+    for (int i = 0; i < io_bound_procs; i++)
+    {
+        pid = fork();
+        if (pid == 0)
+        {
             exec("io_bound", 0);
             exit();
         }
+        total_exec_times[i] = uptime() - t_start;
     }
 
     // Esperar processos filhos finalizarem e coletar métricas (latência de IO, vazão, etc.)
-    for (int i = 0; i < cpu_bound_procs + io_bound_procs; i++) {
+    for (int i = 0; i < cpu_bound_procs + io_bound_procs; i++)
+    {
         wait();
     }
 
+    t_end = uptime();
+    int time = t_end - t_start;
+    int throughput = (time > 0 ? (total_process * 100) / time : 0);
     // Calcular métricas e exibir o desempenho geral do sistema
     printf(1, "Rodada concluída\n");
+    return throughput;
 }
 
-int main(int argc, char *argv[]) {
+float fairness(int *exec_times, int total_process)
+{
+    int sum_x = 0;
+    int sum_x_squared = 0;
+
+    for (int i = 0; i < total_process; i++)
+    {
+        sum_x += exec_times[i];
+        sum_x_squared += exec_times[i] * exec_times[i];
+    }
+    float fair = (float)sum_x / (total_processes * sum_x_squared);
+    return fair;
+}
+
+int main(int argc, char *argv[])
+{
     int cpu_bound_procs = atoi(argv[1]);
     int io_bound_procs = 20 - cpu_bound_procs;
+    int min_throughput = 1000000;
+    int max_throughput = 0;
+    int total_processes = cpu_bound_procs + io_bound_procs;
+    int exec_times[total_processes];
+    float fair;
 
-    for (int round = 0; round < 30; round++) {
+    for (int round = 0; round < 30; round++)
+    {
         printf(1, "Rodada %d\n", round + 1);
-        run_experiment(cpu_bound_procs, io_bound_procs);
+        throughput = run_experiment(cpu_bound_procs, io_bound_procs, exec_times);
+
+        // Throughput
+        if (throughput < min_throughput)
+        {
+            min_throughput = throughput;
+        }
+        if (throughput > max_throughput)
+        {
+            max_throughput = throughput;
+        }
+        float normalized_throughput = 1.0 - ((float)(throughput - min_throughput) / (max_throughput - min_throughput));
+        printf(1, "Vazão Normalizada: %.2f\n", normalized_throughput);
+
+        // Fairness
+        fair = fairness(exec_times, total_processes);
     }
     exit();
 }
